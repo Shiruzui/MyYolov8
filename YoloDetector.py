3cb2848a31187029400cab1d9dfa88ed2be52182
fsitta <fsittaf@yahoo.com>
2024-05-31 14:46:16 -0300

    Changes in readme

--- YoloDetector.py
+++ YoloDetector.py
@@ -1,8 +1,10 @@
 import cv2
+import numpy as np
 import ultralytics
 import torch
 import logging
 import os
+import mss
 
 from plot_boxes import plot_bboxes
 
@@ -15,7 +17,7 @@ logging.info(cv2.__version__)
 
 
 class YOLODetector:
-    def __init__(self, yolov8_path: str = 'yolov8x.pt', conf: float = 0.5, filter_labels: set[str] | None = None,
+    def __init__(self, yolov8_path: str = 'yolov10x.pt', conf: float = 0.5, filter_labels: set[str] | None = None,
                  labels=None, colors=None, output_dir='output'):
         self.config = {
             'conf': conf,
@@ -28,10 +30,10 @@ class YOLODetector:
         self.model = self.get_model()
         os.makedirs(output_dir, exist_ok=True)
 
-    def get_model(self) -> ultralytics.YOLO:
+    def get_model(self) -> ultralytics.YOLOv10:
         if torch.cuda.is_available():
             torch.cuda.set_device(0)
-        return ultralytics.YOLO(self.yolov8_path)
+        return ultralytics.YOLOv10(self.yolov8_path)
 
     def update_config(self, **kwargs):
         for key, value in kwargs.items():
@@ -42,10 +44,13 @@ class YOLODetector:
             print(f"{key}: {value}")
         print(f"Output directory: {self.output_dir}")
 
-    def initialize_video_writer(self, cap, file_name):
+    def initialize_video_writer(self, cap_or_width, height=None, file_name="output.mp4"):
+        if height is not None:
+            width = cap_or_width
+        else:
+            width = int(cap_or_width.get(cv2.CAP_PROP_FRAME_WIDTH))
+            height = int(cap_or_width.get(cv2.CAP_PROP_FRAME_HEIGHT))
         fourcc = cv2.VideoWriter.fourcc(*'mp4v')
-        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         out_path = os.path.join(self.output_dir, file_name)
         out = cv2.VideoWriter(out_path, fourcc, 20.0, (width, height), True)
         if not out.isOpened():
@@ -71,7 +76,7 @@ class YOLODetector:
         cv2.destroyAllWindows()
 
     def process_stream(self, cap, save_output, file_name):
-        out = self.initialize_video_writer(cap, file_name) if save_output else None
+        out = self.initialize_video_writer(cap, None, file_name) if save_output else None
         try:
             while True:
                 ret, frame = cap.read()
@@ -93,6 +98,44 @@ class YOLODetector:
             else:
                 cv2.imwrite(os.path.join(self.output_dir, file_name), frame)
 
+    def run_screen_video(self, save_output=False, file_name="screen_output.mp4", bbox=None, show_border=True):
+        with mss.mss() as sct:
+            if bbox is None:
+                monitor = sct.monitors[1]
+            else:
+                x1, y1, x2, y2 = bbox
+                monitor = {'top': y1, 'left': x1, 'width': x2 - x1, 'height': y2 - y1}
+
+            if save_output:
+                out = self.initialize_video_writer(monitor['width'], monitor['height'], file_name)
+                if out is None:
+                    return
+            else:
+                out = None
+
+            cv2.namedWindow('Object Detection', cv2.WINDOW_NORMAL)
+            cv2.resizeWindow('Object Detection', 800, 600)
+
+            try:
+                while True:
+                    sct_img = sct.grab(monitor)
+                    frame = np.array(sct_img, dtype=np.uint8)[..., :3]
+                    frame = np.ascontiguousarray(frame)
+
+                    if show_border:
+                        cv2.rectangle(frame, (0, 0), (frame.shape[1] - 1, frame.shape[0] - 1), (0, 0, 255), 2)
+
+                    self.process_frame(frame, save_output, file_name, out)
+
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+
+            finally:
+                if save_output and out is not None:
+                    out.release()
+                cv2.destroyAllWindows()
+                cv2.waitKey(1)
+
     def process_frame(self, frame, save_output, file_name, out=None):
         results = self.model.predict(frame)
         plot_bboxes(frame, results[0].boxes.data, **self.config)
@@ -103,5 +146,6 @@ class YOLODetector:
 if __name__ == '__main__':
     detector = YOLODetector()
     detector.show_config()
-    detector.run_video('videos/carolsdogshort.mp4', save_output=True)
-    detector.run_image('images/gato.jpeg', save_output=True)
+    # detector.run_video('videos/seu_video.mp4', save_output=True)
+    # detector.run_image('images/gato.jpeg', save_output=True)
+    detector.run_screen_video()
---
+++ readme.md
@@ -0,0 +1,78 @@
+
+# YOLO Object Detection System
+
+This Python-based project enables object detection using YOLOv10 and YOLOv8 models from the ultralytics library, with capabilities for processing videos, webcam feeds, and images.
+
+## Installation
+
+### Prerequisites
+- Python 3.8 or later
+- OpenCV
+- Numpy
+- PyTorch
+- ultralytics YOLO models
+
+### Setup
+
+1. **YOLOv10 Installation**
+   ```bash
+   pip install -q git+https://github.com/THU-MIG/yolov10.git
+   ```
+
+2. **YOLOv8 Installation**
+   For YOLOv8, use the ultranalytics library:
+   ```bash
+   pip install ultralytics
+   ```
+
+## YOLODetector Class Configuration
+
+### Initialization Parameters
+- `yolov8_path` (str): Path to the model file (e.g., 'yolov10x.pt').
+- `conf` (float): Confidence threshold for detecting objects. Default is 0.5.
+- `filter_labels` (set[str] | None): Set of labels to filter detections. `None` allows all labels.
+- `labels` (dict): Optional dictionary mapping label indices to string descriptions.
+- `colors` (dict): Optional dictionary specifying colors for each label in RGB.
+- `output_dir` (str): Directory where output files will be saved.
+
+### Methods
+
+- **`get_model()`**: Loads the YOLO model based on the provided model path and device availability (GPU/CPU).
+
+- **`update_config(**kwargs)`**: Updates the detector configuration dynamically during runtime. You can pass any configuration parameters (e.g., `conf`, `filter_labels`) as keyword arguments.
+
+- **`show_config()`**: Prints the current configuration settings to the console.
+
+- **`initialize_video_writer(cap, file_name)`**: Initializes a video writer for saving output videos, based on the input capture device and desired output file name.
+
+- **`run_video(video_path, save_output)`**: Processes a video file for object detection. If `save_output` is True, the output is saved using the initialized video writer.
+
+- **`run_webcam(save_output)`**: Processes webcam input for object detection and optionally saves the output.
+
+- **`run_image(image_path, save_output)`**: Processes a single image for object detection and optionally saves the output.
+
+- **`process_stream(cap, save_output, file_name)`**: Handles the continuous input from a video capture device (video file or webcam) and processes each frame through the detector.
+
+- **`save_frame_or_image(frame, save_output, file_name, out=None)`**: Saves a single frame or image, either by writing it to a video or saving it as an image file, depending on the configuration.
+
+- **`process_frame(frame, save_output, file_name, out=None)`**: Processes a single frame for object detection, displays the results, and saves the output if required.
+
+## Additional Features
+
+### Coco Labels and Colors
+
+Utilize the `CocoInfo` class to manage COCO dataset labels and custom color patterns for bounding boxes.
+
+```
+coco_info = CocoInfo()
+coco_labels = coco_info.get_coco_labels()
+coco_colors = coco_info.get_coco_colors_pattern(default=True)
+```
+
+## Contributing
+
+Contributions are welcome. Ensure to follow the existing code style and include tests for new or updated functionality.
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details.
